4b034ad235d9920cfce8b72e19b1accc1568de60
HyDiscord <74053497+HyDiscord@users.noreply.github.com>
2020-12-05 18:57:05 +1100

    Update help.js

--- Commands/help.js
+++ Commands/help.js
@@ -1,18 +1,13 @@
 const Discord = require('discord.js');
 var color = '0x738ADB';
 var footer = 'HyDiscord - Made by cxntered';
-const helpprefix = new Discord.MessageEmbed()
-.setTitle('Help')
-.setDescription("`h!` is the default prefix, so if the commands don't work, check what's the prefix for the server you're currently in.")
-.setColor(color)
-.setFooter(footer)
 module.exports = {
     name: 'help',
     execute(message, args){
         if(!args[0]){
         const embed = new Discord.MessageEmbed()
         .setTitle('Help')
-        .addField('Utility 🛠', '`!help` \n`!members` \n`!ping` \n`!invite` \n`h!clear` \n`h!ban` \n`h!kick`', true) 
+        .addField('Utility 🛠', '`h!help` \n`h!members` \n`h!ping` \n`h!invite` \n`h!clear` \n`h!ban` \n`h!kick`', true) 
         .addField('Hypixel 🎮', '`h!bedwars` \n`h!skywars` \n`h!duels` \n`h!uhc` \n`h!speeduhc` \n`h!watchdog` \n`h!socials` \n`h!namehistory`', true)
         .setDescription('Do `!help [command]` to see what that command does.')
         .setColor(color)
@@ -24,7 +19,7 @@ module.exports = {
             case 'ping':
                 const ping = new Discord.MessageEmbed()
                 .setTitle('Help')
-                .addField('h!ping', '`!ping` will send a "Pong!" along with your current ping.')
+                .addField('h!ping', '`h!ping` will send a "Pong!" along with your current ping.')
                 .setColor(color)
                 .setFooter(footer)
                 message.channel.send('I sent you a DM containing the information!')
@@ -33,7 +28,7 @@ module.exports = {
             case 'members':
                 const members = new Discord.MessageEmbed()
                 .setTitle('Help')
-                .addField('h!members', '`!members` will send you the current member count of the server you are in.')
+                .addField('h!members', '`h!members` will send you the current member count of the server you are in.')
                 .setColor(color)
                 .setFooter(footer)
                 message.channel.send('I sent you a DM containing the information!')
@@ -42,7 +37,7 @@ module.exports = {
             case 'invite':
                 const invite = new Discord.MessageEmbed()
                 .setTitle('Help')
-                .addField('h!invite', '`!invite` will send you a link to invite the bot aswell as a link to join the HyDiscord server')
+                .addField('h!invite', '`h!invite` will send you a link to invite the bot aswell as a link to join the HyDiscord server')
                 .setColor(color)
                 .setFooter(footer)
                 message.channel.send('I sent you a DM containing the information!')
@@ -51,7 +46,7 @@ module.exports = {
             case 'ban':
                 const ban = new Discord.MessageEmbed()
                 .setTitle('Help')
-                .addField('h!ban', '`!ban` will ban a user you specify')
+                .addField('h!ban', '`h!ban` will ban a user you specify')
                 .setColor(color)
                 .setFooter(footer)
                 message.channel.send('I sent you a DM containing the information!')
@@ -60,7 +55,7 @@ module.exports = {
             case 'kick':
                 const kick = new Discord.MessageEmbed()
                 .setTitle('Help')
-                .addField('h!kick', '`!kick` will kick a user you specify')
+                .addField('h!kick', '`h!kick` will kick a user you specify')
                 .setColor(color)
                 .setFooter(footer)
                 message.channel.send('I sent you a DM containing the information!')
@@ -69,7 +64,7 @@ module.exports = {
             case 'clear':
                 const clear = new Discord.MessageEmbed()
                 .setTitle('Help')
-                .addField('h!clear', '`!clear` will clear a specific number of messages you specify')
+                .addField('h!clear', '`h!clear` will clear a specific number of messages you specify')
                 .setColor(color)
                 .setFooter(footer)
                 message.channel.send('I sent you a DM containing the information!')
@@ -78,7 +73,7 @@ module.exports = {
             case 'player':
                 const player = new Discord.MessageEmbed()
                 .setTitle('Help')
-                .addField('h!player', '`!player` will show you the Hypixel stats of a user')
+                .addField('h!player', '`h!player` will show you the Hypixel stats of a user')
                 .setColor(color)
                 .setFooter(footer)
                 message.channel.send('I sent you a DM containing the information!')
@@ -88,7 +83,7 @@ module.exports = {
             case 'bw':
                 const bedwars = new Discord.MessageEmbed()
                 .setTitle('Help')
-                .addField('h!bedwars', '`!bedwars` will show you the BedWars stats of a user')
+                .addField('h!bedwars', '`h!bedwars` will show you the BedWars stats of a user')
                 .addField('Aliases', '`h!bw`')
                 .setColor(color)
                 .setFooter(footer)
@@ -99,7 +94,7 @@ module.exports = {
             case 'sw':
                 const skywars = new Discord.MessageEmbed()
                 .setTitle('Help')
-                .addField('h!skywars', '`!skywars` will show you the SkyWars stats of a user')
+                .addField('h!skywars', '`h!skywars` will show you the SkyWars stats of a user')
                 .addField('Aliases', '`h!sw`')
                 .setColor(color)
                 .setFooter(footer)
@@ -109,7 +104,7 @@ module.exports = {
             case 'uhc':
                 const uhc = new Discord.MessageEmbed()
                 .setTitle('Help')
-                .addField('h!uhc', '`!uhc` will show you the UHC stats of a user')
+                .addField('h!uhc', '`h!uhc` will show you the UHC stats of a user')
                 .setColor(color)
                 .setFooter(footer)
                 message.channel.send('I sent you a DM containing the information!')
@@ -119,7 +114,7 @@ module.exports = {
             case 'suhc':
                 const speeduhc = new Discord.MessageEmbed()
                 .setTitle('Help')
-                .addField('h!speeduhc', '`!speeduhc` will show you the SpeedUHC stats of a user')
+                .addField('h!speeduhc', '`h!speeduhc` will show you the SpeedUHC stats of a user')
                 .addField('Aliases', '`h!suhc`')
                 .setColor(color)
                 .setFooter(footer)
@@ -130,7 +125,7 @@ module.exports = {
             case 'wdr':
                 const watchdog = new Discord.MessageEmbed()
                 .setTitle('Help')
-                .addField('h!watchdog', '`!watchdog` will show you Watchdog stats')
+                .addField('h!watchdog', '`h!watchdog` will show you Watchdog stats')
                 .addField('Aliases', '`h!wdr`')
                 .setColor(color)
                 .setFooter(footer)
@@ -141,7 +136,7 @@ module.exports = {
             case 'nh':
                 const namehistory = new Discord.MessageEmbed()
                 .setTitle('Help')
-                .addField('h!namehistory', '`!namehistory` will show you the name history of a user')
+                .addField('h!namehistory', '`h!namehistory` will show you the name history of a user')
                 .addField('Aliases', '`h!nh`')
                 .setColor(color)
                 .setFooter(footer)
